ae790d11aa81bb134a07bef9a847f3bf711a46b2
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-06-01 15:53:53 +0300

    4th task

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -10,8 +10,6 @@ from models.place import Place
 from models.state import State
 
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -20,7 +18,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -36,11 +34,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('1-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid.uuid4())
+                           cache_id=str(uuid.uuid4()))
 
 
 if __name__ == "__main__":
